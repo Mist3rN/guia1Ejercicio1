d4274223103d8d2ee3bc74282958d97a56514d62
EmitoStuer <stuer.87@gmail.com>
2023-08-08 12:45:39 -0300

    Ariel aca te hice un sout
    Joya, funciona bien.

--- guia1Ejercicio1/src/guia1ejercicio1/Guia1Ejercicio1.java
+++ guia1Ejercicio1/src/guia1ejercicio1/Guia1Ejercicio1.java
@@ -7,6 +7,7 @@ public class Guia1Ejercicio1 {
    
     public static void main(String[] args) {
         System.out.println("funciono?");
+        System.out.println("Perfecto ariel, funcionó");
     }
     
 }
